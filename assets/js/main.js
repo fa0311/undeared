6cff091789da597a7cdf886ede23621ffc07bf31
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-09-15 00:57:41 +0900

    update event整理

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ class mainClass {
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas
         });
-        this.eventset();
         this.renderer.shadowMap.enabled = true;
 
         // this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -17,8 +16,8 @@ class mainClass {
             1,
             1000
         );
-        this.resizefunc();
 
+        new EventListener(this.canvas, this.renderer, this.camera);
         if (config.stats) {
             this.stats = new Stats();
             this.stats.showPanel(0);
@@ -283,69 +282,6 @@ class mainClass {
             element.event(this.camera);
         });
         this.renderer.render(this.scene, this.camera);
-    }
-    eventset() {
-        this.canvas.addEventListener("mousedown", this.canvasclickfunk);
-        document.addEventListener('pointerlockchange', () => this.pointerlockchangefunc());
-        window.addEventListener('resize', () => this.resizefunc());
-    }
-    canvasclickfunk(event) {
-        let canvas_target = event.target;
-        canvas_target.requestPointerLock = canvas_target.requestPointerLock || canvas_target.mozRequestPointerLock || canvas_target.webkitRequestPointerLock;
-        canvas_target.requestPointerLock();
-    }
-
-    pointerlockchangefunc() {
-        if (document.pointerLockElement || document.mozPointerLockElement || document.webkitPointerLockElement) {
-            this.canvas.addEventListener("mousedown", this.mousedownfunk);
-            this.canvas.addEventListener("mouseup", this.mouseupfunc);
-            document.addEventListener("keydown", this.keydownfunc);
-            document.addEventListener("keyup", this.keyupfunc);
-            document.addEventListener("mousemove", this.mousemovefunc);
-        } else {
-            this.canvas.removeEventListener("mousedown", this.mousedownfunk);
-            this.canvas.removeEventListener("mouseup", this.mouseupfunc);
-            document.removeEventListener("keydown", this.keydownfunc);
-            document.removeEventListener("keyup", this.keyupfunc);
-            document.removeEventListener("mousemove", this.mousemovefunc);
-        }
-    }
-    mousedownfunk(event) {
-        if (event.button == 0)
-            mousedata.left = true;
-        else if (event.button == 2)
-            mousedata.right = true;
-    }
-    mouseupfunc(event) {
-        if (event.button == 0)
-            mousedata.left = false;
-        else if (event.button == 2)
-            mousedata.right = false;
-    }
-    keydownfunc(event) {
-        let action = config.keycode[String(event.keyCode)];
-        console.log(action);
-        if (action === undefined)
-            return
-        keydata.action[action] = true;
-    }
-    keyupfunc(event) {
-        let action = config.keycode[String(event.keyCode)];
-        if (action === undefined)
-            return
-        keydata.action[action] = false;
-    }
-    mousemovefunc(event) {
-        mousedata.x += event.movementX / 1000;
-        mousedata.y += event.movementY / 1000;
-    }
-    resizefunc() {
-        let width = window.innerWidth;
-        let height = window.innerHeight;
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(width, height);
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
     }
 }
 
