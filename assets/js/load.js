6ccf4c1a40781098550dd0ab56285a55a3efb4f0
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-09-26 16:18:23 +0900

    delete model

--- assets/js/load.js
+++ assets/js/load.js
@@ -8,15 +8,7 @@ window.addEventListener("DOMContentLoaded",
 class loaderClass {
     constructor(loadedfn) {
         let modelpath = {
-            "bookshelf": "assets/models/bookshelf/bookshelf.glb",
-            "window": "assets/models/window/window.glb",
-            "wall": "assets/models/wall/wall.glb",
-            "wall_front": "assets/models/wall/wall_front.glb",
-            "door_frame": "assets/models/door/door_frame.glb",
-            "door_frame_front": "assets/models/door/door_frame_front.glb",
-            "door_front": "assets/models/door/door_front.glb",
-            "door_room": "assets/models/door/door_room.glb",
-            "untitled": "assets/models/window/untitled.glb",
+            "main": "assets/models/home/main.glb",
         };
         this.loaded = 0;
         this.len = Object.keys(modelpath).length;
